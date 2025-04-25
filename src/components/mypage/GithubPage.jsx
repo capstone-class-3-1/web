800be6e658094172303b323d40cba5d8d8a26920
lioba00700 <lioba00700@naver.com>
2025-04-26 02:14:11 +0900

    chore: 깃허브 잔디 react-github-calendar 사용

--- src/components/mypage/GithubPage.jsx
+++ src/components/mypage/GithubPage.jsx
@@ -1,10 +1,11 @@
 import '../../styles/components/mypage/GithubPage.css'
+import GitHubCalendar from 'react-github-calendar'
 
 const GithubPage = () => {
     return(
         <div className="GithubPage">
             <p>내 칭호</p>
-            <img src="https://ghchart.rshah.org/jinhyeon-dev" />
+            <GitHubCalendar username='lioba00700'/>
         </div>
     )
 }
