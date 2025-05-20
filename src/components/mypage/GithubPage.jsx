fec932aacb21e927b1ee29104033eb7361d905fb
lioba00700 <lioba00700@naver.com>
2025-05-20 14:52:21 +0900

    chore: 깃허브 잔디 수정 - 잔디 밝은 테마로 변경 - 폰트 크기 변경 - react-tooltip 라이브러리 활용해 날짜별 commit 개수 표시

--- src/components/mypage/GithubPage.jsx
+++ src/components/mypage/GithubPage.jsx
@@ -1,11 +1,24 @@
+import React, { useEffect, useState } from 'react'
 import '../../styles/components/mypage/GithubPage.css'
 import GitHubCalendar from 'react-github-calendar'
+import { Tooltip } from 'react-tooltip'
 
 const GithubPage = () => {
+    const [isLoading, setIsLoading] = useState(false);
     return(
         <div className="GithubPage">
             <p>내 칭호</p>
-            <GitHubCalendar username='lioba00700'/>
+            <GitHubCalendar 
+                username='lioba00700'
+                fontSize={12}
+                colorScheme='light'
+                renderBlock={(block, activity) => React.cloneElement(block, {
+                    "data-tooltip-id":"tooltip",
+                    "data-tooltip-html": `${activity.date}<br />${activity.count} commits`,
+                    "data-tooltip-variant": "dark",
+                })}
+            />
+            <Tooltip id="tooltip" className='github_tooltip'/>
         </div>
     )
 }
