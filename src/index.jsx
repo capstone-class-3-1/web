a89905c5d2367ff6b9de3c478088290cf25368ed
lioba00700 <lioba00700@naver.com>
2025-06-10 16:28:56 +0900

    chore: 빌드 에러 수정 - 잘못된 persistor import 수정

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider, persistor } from 'react-redux';
-import { store } from './store/store';
+import { Provider } from 'react-redux';
+import { store, persistor } from './store/store';
 import {PersistGate} from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
