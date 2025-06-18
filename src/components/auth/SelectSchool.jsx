06996019a6bde6dffabe873fc94b0a7862b1b3c7
lioba00700 <lioba00700@naver.com>
2025-06-19 00:29:51 +0900

    chore: 텍스트 및 스타일 수정 - 회원가입 고등학교 선택 옵션에서 'ㅇㅇㅇ고등학교'를 'ㅇㅇㅇ고'로 축약 - 메인 화면의 랭킹 제목을 '월간랭킹'으로 변경 - 로그아웃 버튼 CSS에서 커서를 포인터로 설정

--- src/components/auth/SelectSchool.jsx
+++ src/components/auth/SelectSchool.jsx
@@ -4,11 +4,11 @@ const SelectSchool = ({handle, data}) => {
     return(
         <div className="SelectSchool">
             <select value={data} onChange={(e) => handle('school', e.target.value)}>
-                <option value={'경북소프트웨어마이스터고등학교'}>경북소프트웨어마이스터고등학교</option>
-                <option value={'대구소프트웨어마이스터고등학교'}>대구소프트웨어마이스터고등학교</option>
-                <option value={'대덕소프트웨어마이스터고등학교'}>대덕소프트웨어마이스터고등학교</option>
-                <option value={'부산소프트웨어마이스터고등학교'}>부산소프트웨어마이스터고등학교</option>
-                <option value={'광주소프트웨어마이스터고등학교'}>광주소프트웨어마이스터고등학교</option>
+                <option value={'경북소프트웨어마이스터고'}>경북소프트웨어마이스터고등학교</option>
+                <option value={'대구소프트웨어마이스터고'}>대구소프트웨어마이스터고등학교</option>
+                <option value={'대덕소프트웨어마이스터고'}>대덕소프트웨어마이스터고등학교</option>
+                <option value={'부산소프트웨어마이스터고'}>부산소프트웨어마이스터고등학교</option>
+                <option value={'광주소프트웨어마이스터고'}>광주소프트웨어마이스터고등학교</option>
             </select>
         </div>
     )
