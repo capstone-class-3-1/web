80122f4ca869ecc95ceacf47b907160e2179e830
lioba00700 <lioba00700@naver.com>
2025-06-19 00:43:41 +0900

    feat: 마이페이지 API 연결 - `username` 및 사용자 Issue와 PR 개수 데이터 받기 - 칭호 API 연결

--- src/components/mypage/BadgeItem.jsx
+++ src/components/mypage/BadgeItem.jsx
@@ -1,12 +1,12 @@
 import '../../styles/components/mypage/BadgeItem.css'
 import colorCode from '../../utils/color';
 
-const BadgeItem = () => {
+const BadgeItem = ({name, level, imageUrl}) => {
     return(
         <div className="BadgeItem">
-            <div className="badge_icon"></div>
-            <p className="badge_name">이슈해결왕</p>
-            <p className="badge_level" style={{color:colorCode.pointBlue}}>Lv.5</p>
+            <div className="badge_icon"><img src={imageUrl} alt="칭호이미지" /></div>
+            <p className="badge_name">{name}</p>
+            <p className="badge_level" style={{color:colorCode.pointBlue}}>Lv.{level}</p>
         </div>
     )
 }
